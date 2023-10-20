e372b9deaed3c3608edf8f90b7e2ccd0673ef946
yayoiqz <lo9808ve@qq.com>
2023-10-20 18:09:34 +0800

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.scss', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),// 源文件根目录
+    }
+  },
 })
